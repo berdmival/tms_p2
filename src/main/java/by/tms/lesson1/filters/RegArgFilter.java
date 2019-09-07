ca24041039d02f4491c880128412c683925c6109
berdmival <mtsby7049874@gmail.com>
2019-09-08 00:44:07 +0300

    reg arguments filter is ready

--- src/main/java/by/tms/lesson1/filters/RegArgFilter.java
+++ src/main/java/by/tms/lesson1/filters/RegArgFilter.java
@@ -8,10 +8,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static by.tms.lesson1.html_fragmets.Templates.*;
+
 @WebFilter(filterName = "regArgFilter", servletNames = "regServlet")
 public class RegArgFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        chain.doFilter(req, res);
+        if ((req.getParameter("name") != null) & (req.getParameter("age") != null) & (req.getParameter("password") != null)) {
+            chain.doFilter(req, res);
+        } else {
+            res.getWriter().println(HTML_HEADER);
+            res.getWriter().println(REGISTER_REQUEST);
+            res.getWriter().println(HTML_FOOTER);
+        }
     }
 }

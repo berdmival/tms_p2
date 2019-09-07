8dbb6f431aa60e307867d41e82f33a4f5f7b6d69
berdmival <mtsby7049874@gmail.com>
2019-09-08 00:46:13 +0300

    auth arguments filter is ready

--- src/main/java/by/tms/lesson1/filters/AuthArgFilter.java
+++ src/main/java/by/tms/lesson1/filters/AuthArgFilter.java
@@ -8,10 +8,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static by.tms.lesson1.html_fragmets.Templates.*;
+
 @WebFilter(filterName = "authArgFilter", servletNames = "authServlet")
 public class AuthArgFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        chain.doFilter(req, res);
+        if ((req.getParameter("name") != null) & (req.getParameter("password") != null)) {
+            chain.doFilter(req, res);
+        } else {
+            res.getWriter().println(HTML_HEADER);
+            res.getWriter().println(AUTH_FAIL);
+            res.getWriter().println(HTML_FOOTER);
+        }
     }
 }

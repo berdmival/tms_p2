686694d145e487d10b531757c31876a5a6e94ec1
berdmival <mtsby7049874@gmail.com>
2019-09-09 17:31:29 +0300

    replace line in templates with <hr> tag

--- src/main/java/by/tms/lesson1/html_fragmets/Templates.java
+++ src/main/java/by/tms/lesson1/html_fragmets/Templates.java
@@ -24,7 +24,7 @@ public interface Templates {
     String REGISTER_REQUEST = "<H1>Please, enter your name, age and password in the request http://yourserver:port/index/reg?name=[yourName]&age=[yourAge]&password=[yourPassword]</H1>";
     String CURRENT_REQUEST_IS_INCORRECT = "<H3>Current request is incorrect!</H3>";
     String SESSIONS_MAP_IS_EMPTY = "Sessions map is empty<br>";
-    String LINE = "_____________________________________________________________________________________________________<br><br>";
+    String LINE = "<hr size=\"2\" color=\"black\">";
     String HISTORY_IS_EMPTY = "History is empty<br>";
     String HISTORY = "<H4>History:</H4>";
     String AUTH_FAIL = "<H1>Name or password is incorrect!</H1>";
